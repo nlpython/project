52049964e75aac5d073a99c7343d6987932adafa
ysh <2994056734@qq.com>
2022-10-10 17:47:38 +0800

    first commit

---
+++ src/main/java/com/yruns/ChangzhengApplication.java
@@ -0,0 +1,13 @@
+package com.yruns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangzhengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChangzhengApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yruns/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.yruns.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+/**
+ * WebMvcConfig
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/");
+    }
+
+}
